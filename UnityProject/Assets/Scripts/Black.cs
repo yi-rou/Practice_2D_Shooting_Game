17e405ff38d075e6f3d4db29c1f0e2a66a2a48b3
yi-rou <57440062+yi-rou@users.noreply.github.com>
2019-12-10 20:40:15 +0800

    已完成

--- UnityProject/Assets/Scripts/Black.cs
+++ UnityProject/Assets/Scripts/Black.cs
@@ -4,6 +4,26 @@ using UnityEngine.UI;
 public class Black : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
-    
+
+    public int HP = 10;
+    public Text textHP;
+    public AudioSource aud;
+    public AudioClip soundhurt;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HP = HP - 1;
+        textHP.text = HP.ToString();
+        aud.PlayOneShot(soundhurt);
+    }
+
+    private void Update()
+    {
+        if (HP <= 0)
+        {
+            Destroy(gameObject,0);
+        }
+    }
+
     #endregion
 }
--- UnityProject/Assets/Scripts/White.cs
+++ UnityProject/Assets/Scripts/White.cs
@@ -3,7 +3,23 @@
 public class White : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
-    
+
+    [Header("子彈")]
+    public GameObject bullet;
+    public AudioSource aud;
+    public AudioClip soundShoot;
+    [Header("子彈發射位置")]
+    public Transform point;
+
+    private void Update()
+    {
+        Vector3 p = point.position;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(bullet,p,Quaternion.identity);
+            aud.PlayOneShot(soundShoot);
+        }
+    }
     #endregion
 
     #region KID 區域 - 不要偷看 @3@
